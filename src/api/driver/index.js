2a5924f0f990d1a69e1ef1663aebc3d5d4a4c7b6
Linxhcy <2452942350@qq.com>
2023-05-24 21:54:46 +0800

    司机行程页面初步展示

--- src/api/driver/index.js
+++ src/api/driver/index.js
@@ -24,6 +24,19 @@ export const addCarpooling = async (carpoolingDto) => {
     return false
 }
 
+export const updateCarpooling = async (carpoolingDto) => {
+	beginLoading('正在上传拼车信息')
+	try{
+		const {data} = await axios.post('/api/carpooling/driver/carpooling', carpoolingDto)
+		return data
+	} catch (e) {
+		showNotify({type: 'danger', message: `修改拼车信息失败,${e.message}`})
+	} finally {
+		closeToast()
+	}
+	return false
+}
+
 export const handleGetDriversCarpoolingList = async (queryDto) => {
 	beginLoading('正在加载拼车信息列表')
 	try {
