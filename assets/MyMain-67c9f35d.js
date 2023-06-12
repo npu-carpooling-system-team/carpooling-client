373e03a0bc0ae3006c4ee917f85206f532cd52b1
tanknok <tanknok@users.noreply.github.com>
2023-06-12 07:13:08 +0000

    deploy: 39c4d37ffca6bdebec121c48a7fee5209e5268d9

---
+++ assets/MyMain-67c9f35d.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index-335d2d88.js";import{r,a as t,c}from"./.pnpm-ba0d9b51.js";const n={};function s(_,a){const e=r("router-view");return t(),c(e)}const m=o(n,[["render",s]]);export{m as default};
