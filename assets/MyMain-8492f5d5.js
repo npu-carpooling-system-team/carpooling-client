0cc7f1764c1ca14e19bace860d6d5fddbf37795f
WangMinan <WangMinan@users.noreply.github.com>
2023-06-17 14:31:51 +0000

    deploy: 330991f59104f736553d9f064ed68cc23a8d0559

---
+++ assets/MyMain-8492f5d5.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index-a9245716.js";import{r,a as t,c}from"./.pnpm-2d117cc4.js";const n={};function s(_,a){const e=r("router-view");return t(),c(e)}const m=o(n,[["render",s]]);export{m as default};
